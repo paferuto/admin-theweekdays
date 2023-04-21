68f923b5e66c785bf0bdedbbf2b87887c7eb4ee8
Thanh <thanhenc@gmail.com>
2023-04-22 03:07:48 +0700

    vi language sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormatService } from 'src/services/format.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'admin';
+
+  constructor(public _format: FormatService) {}
 }
