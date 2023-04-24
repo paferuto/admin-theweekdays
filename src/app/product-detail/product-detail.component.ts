a17989d32195641216d8841c1323c2221ff52189
paferuto <haodh20411@st.uel.edu.vn>
2023-04-24 12:06:17 +0700

    product chua hoan chinh

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from 'src/services/product.service';
 import { Product } from '../product';
-import { variant } from '../product';
+import { CategoryService } from 'src/services/category.service';
 
 @Component({
   selector: '[app-product-detail]',
@@ -14,18 +14,50 @@ export class ProductDetailComponent implements OnInit {
   product: Product = new Product();
   errMessage: any;
   divClass: any;
-  constructor(private route: ActivatedRoute, private _service: ProductService) { }
+  category: any;
+  categories: any;
+  constructor(private route: ActivatedRoute, private _service: ProductService, private category_service: CategoryService) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
     this._service.getProductById(this.id).subscribe({
       next: (data) => {
         this.product = data,
-        this.showDiv();
+          this.showDiv(),
+          this.category_service.getCategoryById(data.category).subscribe({
+            next: (data1) => {
+              this.category = data1;
+            },
+            error: (err) => { this.errMessage = err }
+          });
+      },
+      error: (err) => { this.errMessage = err }
+    });
+
+    // this.category_service.getCategoryById(this.product.category).subscribe({
+    //   next: (data) => {
+    //     this.category = data;
+    //   },
+    //   error: (err) => { this.errMessage = err }
+    // });
+    this.category_service.getCategories().subscribe({
+      next: (data) => {
+        this.categories = data;
+        console.log(this.categories);
       },
       error: (err) => { this.errMessage = err }
     });
   }
+
+  getCurrentCategory(id:string) {
+    this.category_service.getCategoryById(id).subscribe({
+      next: (data) => {
+        this.category = data;
+      },
+      error: (err) => { this.errMessage = err }
+    });
+  }
+
 
   showDiv() {
     if (this.product.on_sale == true) {
@@ -47,7 +79,7 @@ export class ProductDetailComponent implements OnInit {
       alert("Minimum quantity cannot be higher than maximum quantity");
       return;
     }
-    if (this.product.on_sale == false){
+    if (this.product.on_sale == false) {
       this.product.price = this.product.original_price;
     }
     for (let i = 0; i < this.product.variants.length; i++) {
@@ -68,4 +100,21 @@ export class ProductDetailComponent implements OnInit {
       error: (err) => { this.errMessage = err }
     });
   }
+
+  onFileSelected(event: any) {
+    if (event.target.files && event.target.files.length) {
+      const file = event.target.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        const base64 = reader.result as string;
+        this.product.image.push(base64);
+      };
+    }
+  }
+
+  deleteImage(index: number) {
+    this.product.image.splice(index, 1);
+  }
+
 }
